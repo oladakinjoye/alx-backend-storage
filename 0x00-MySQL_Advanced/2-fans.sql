eece4c49ce1fc17b4dd9d0d9022ae7a6f2315abb
oladakinjoye <oladakinjoye@gmail.com>
2024-07-12 16:31:02 -0700

    update 2 fan

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,16 +1,5 @@
--- Create a temporary table with the origin and number of fans
-CREATE TEMPORARY TABLE band_origins AS
-SELECT origin, COUNT(*) AS nb_fans
-FROM metal_bands
-GROUP BY origin;
-
--- Rank the origins by the number of fans
-SELECT 
-    origin, 
-    nb_fans,
-    DENSE_RANK() OVER (ORDER BY nb_fans DESC) AS rank
-FROM band_origins
-ORDER BY rank;
-
--- Drop the temporary table
-DROP TABLE band_origins;
+-- Ranks country origins of bands, ordered by the number of (non-unique) fans.
+SELECT origin, SUM(fans) AS nb_fans
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
